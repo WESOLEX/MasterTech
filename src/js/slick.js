a78c45bf902deb6390f88c3a079b040ee6161d55
Alex <alewes2000@wp.pl>
2023-11-01 20:02:04 +0100

    dodanie szkicu galerii

--- src/js/slick.js
+++ src/js/slick.js
@@ -1,15 +1,15 @@
 $('.slider-for').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    asNavFor: '.slider-nav'
-  });
-  $('.slider-nav').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    asNavFor: '.slider-for',
-    dots: true,
-    centerMode: true,
-    focusOnSelect: true
-  });
+	slidesToShow: 1,
+	slidesToScroll: 3,
+	arrows: false,
+	fade: true,
+	asNavFor: '.slider-nav',
+});
+$('.slider-nav').slick({
+	slidesToShow: 2,
+	slidesToScroll: 5,
+	asNavFor: '.slider-for',
+	dots: true,
+	centerMode: true,
+	focusOnSelect: true,
+});
