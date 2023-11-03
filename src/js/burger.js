42720f5df8b20c1f71e992945d920e2efd62c93c
Alex <alewes2000@wp.pl>
2023-11-03 20:52:36 +0100

    zrobiona nawigacja, kolorystyka strony głównej

---
+++ src/js/burger.js
@@ -0,0 +1,35 @@
+const burgerBtn = document.querySelector('.burger-btn');
+const closeBtn = document.querySelector('.close-btn');
+const showListOfServicesBtn = document.querySelector('.show-list-of-services-btn');
+let burgerMenu = document.getElementById('#options');
+const burgerMenuElements = document.querySelectorAll('.nav-option-onburger');
+const offerOptionBurger = burgerMenu.querySelector('.offer-option-burger');
+let listOfServices = offerOptionBurger.querySelector('.list-of-services');
+const arrowItem = showListOfServicesBtn.querySelector('.fa-arrow-left');
+
+function burgerMenuClose() {
+	burgerMenu.style.left = '100%';
+	listOfServices.classList.remove('list-of-services-show');
+}
+
+burgerBtn.addEventListener('click', () => {
+	burgerMenu.style.left = '70%';
+});
+
+closeBtn.addEventListener('click', burgerMenuClose);
+burgerMenuElements.forEach(element => {
+	element.addEventListener('click', burgerMenuClose);
+});
+
+showListOfServicesBtn.addEventListener('click', () => {
+	console.log(window.scrollY);
+	listOfServices.classList.toggle('list-of-services-show');
+	arrowItem.classList.toggle('arrow-rotate');
+});
+function hideBurgerMenu() {
+	if (window.scrollY >= 300) {
+		console.log('true');
+		burgerMenuClose();
+	}
+}
+window.addEventListener('scroll', hideBurgerMenu);
